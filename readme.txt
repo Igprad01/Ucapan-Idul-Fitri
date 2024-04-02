616654066ef45ee469564264118edff4989130d4
igprad01 <igustiputra99@gmail.com>
2024-04-02 20:53:00 +0700

    add card

---
+++ readme.txt
@@ -0,0 +1,5 @@
+design nanti 
+fitur 
+- musik takbiran
+- klik open 
+- background idul fitri diblur
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'image' : "url('/public/assets/background.png')",
+      }
+    },
   },
   plugins: [],
 }

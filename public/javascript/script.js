67e6a63c2266b1819455fc0c97f3cf5c73fcb0cf
igprad01 <igustiputra99@gmail.com>
2024-04-06 16:22:16 +0700

    finishing

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,33 +1,38 @@
+
 const words = ["I Gusti Bagus Putra Pradwi Sekeluarga"];
 let i = 0;
 let j = 0;
 let isDeleting = false;
 
-
 function type() {
-    currentWord = words[i];
+    const currentWord = words[i];
     if (isDeleting) {
-      document.getElementById("textwriter").textContent = currentWord.substring(0, j+1);
-      j--;
-      if (j == 0) {
-        isDeleting = false;
-        i++;
-        if (i == words.length) {
-          i = 0;
+        document.getElementById("textwriter").textContent = currentWord.substring(0, j + 1);
+        j--;
+        if (j == 0) {
+            isDeleting = false;
+            i++;
+            if (i == words.length) {
+                i = 0;
+            }
         }
-      }
     } else {
-      document.getElementById("textwriter").textContent = currentWord.substring(0, j-1);
-      j++;
-      if (j == currentWord.length) {
-        isDeleting = true;
-      }
+        document.getElementById("textwriter").textContent = currentWord.substring(0, j - 1);
+        j++;
+        if (j == currentWord.length) {
+            isDeleting = true;
+        }
     }
     setTimeout(type, 100);
-  }
-  
-  type();
+}
 
-  function nyalaMusik() {
-    // isi musik otomatis takbiran
-  }
+type();
+
+function nyalaMusik() {
+  const audio = new Audio("./public/assets/Takbiran.mp3");
+  audio.loop = true; 
+  audio.play();
+}
+
+window.onload = nyalaMusik;
+

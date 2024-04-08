e46de6ce6699a4193bf1129417358ba7a9be88e3
igprad01 <igustiputra99@gmail.com>
2024-04-08 11:20:48 +0700

    finishing

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,39 +1,9 @@
+const musik = document.getElementById('audio');
 
-const words = ["I Gusti Bagus Putra Pradwi Sekeluarga"];
-let i = 0;
-let j = 0;
-let isDeleting = false;
-
-function type() {
-    const currentWord = words[i];
-    if (isDeleting) {
-        document.getElementById("textwriter").textContent = currentWord.substring(0, j + 1);
-        j--;
-        if (j == 0) {
-            isDeleting = false;
-            i++;
-            if (i == words.length) {
-                i = 0;
-            }
-        }
-    } else {
-        document.getElementById("textwriter").textContent = currentWord.substring(0, j - 1);
-        j++;
-        if (j == currentWord.length) {
-            isDeleting = true;
-        }
-    }
-    setTimeout(type, 100);
+function Nyalamusic() {
+    const audio = new Audio("../public/assets/audio/audio.mp3");
+    audio.loop = true;
+    audio.play();
+    musik.removeEventListener("click", Nyalamusic);
 }
-
-type();
-
-function nyalaMusik() {
-  const audio = new Audio("./public/assets/Takbiran.mp3");
-  audio.loop = true; 
-  audio.play();
-}
-
-window.onload = nyalaMusik;
-
-
+musik.addEventListener("click", Nyalamusic);
---
+++ public/javascript/testwriter.js
@@ -0,0 +1,33 @@
+
+const words = ["I Gusti Bagus Putra Pradwi Sekeluarga"];
+let i = 0;
+let j = 0;
+let isDeleting = false;
+
+function type() {
+    const currentWord = words[i];
+    if (isDeleting) {
+        document.getElementById("textwriter").textContent = currentWord.substring(0, j + 1);
+        j--;
+        if (j == 0) {
+            isDeleting = false;
+            i++;
+            if (i == words.length) {
+                i = 0;
+            }
+        }
+    } else {
+        document.getElementById("textwriter").textContent = currentWord.substring(0, j - 1);
+        j++;
+        if (j == currentWord.length) {
+            isDeleting = true;
+        }
+    }
+    setTimeout(type, 100);
+}
+
+type();
+
+
+
+
